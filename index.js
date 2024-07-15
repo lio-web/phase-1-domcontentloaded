bc0153779e518a5a8239389a5ee1dc7c150bfb98
lionard <leonard.muhati@student.moringaschool.com>
2024-07-15 14:31:25 +0300

    assignment compelete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    updateDOM();
+});
+ function updateDOM(){
+    document.querySelector('p')
+    .textContent="This is really cool!";
+}
